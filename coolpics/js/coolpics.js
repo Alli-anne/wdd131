97b1749704517b504e1651929781e672ea2e19a0
Alli_anne <alannieprice@gmail.com>
2024-05-21 16:08:30 -0600

    fixed?

--- coolpics/js/coolpics.js
+++ coolpics/js/coolpics.js
@@ -11,13 +11,19 @@ function viewHandler(event) {
     const clickedElement = event.target;
 
     if (clickedElement.tagName === 'IMG') {
-        const srcArray = clickedElement.imgSrc.split("-");
-        const newImageSrc =`${srcArray[1]}-full.jpeg`;
+        const srcArray = clickedElement.src.split("-");
+        // Correctly construct the new image src
+        const newImageSrc = `${srcArray[0]}-full.jpeg`;
         const altText = clickedElement.alt;
         console.log(srcArray);
         const htmlToInsert = viewerTemplate(newImageSrc, altText);
-        document.body.insertHTML("afterbegin", htmlToInsert);
 
+        // Insert the HTML to display the full-size image
+        const newElement = document.createElement('div');
+        newElement.innerHTML = htmlToInsert;
+        document.body.prepend(newElement);
+
+        // Add the event listener for the close button
         document.querySelector(".close-viewer").addEventListener("click", closeViewer);
     }
 }
